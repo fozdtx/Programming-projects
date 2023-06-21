bfaa19c81d731845e8920a2c476bdfa357a3cf95
fozdtx <135956218+fozdtx@users.noreply.github.com>
2023-06-21 18:56:28 +0530

    Create main.cpp

---
+++ C++/Command Sector/main.cpp
@@ -0,0 +1,171 @@
+// including header files
+
+#include <iostream>
+
+#include <cstdio>
+
+#include <fstream>
+
+// defining stdndard namespace 
+
+using namespace std;
+
+// main function()
+
+// defining the commands for input
+
+string command;
+
+int main(){
+
+	// setting the text color to lime using system function and calling os kernel	system("color A");
+
+	
+
+	
+
+	// welcome message to the console
+
+	cout<<"Welcome to this command scector"<<endl<<endl;
+
+	
+
+	// making a infinite lines generator using for loop
+
+	
+
+	bool line_is_infinite = true;
+
+	
+
+	for (int lines = 1;line_is_infinite = true;lines++){
+
+		
+
+		cout<<lines<<">>>";
+
+		cin>>command;
+
+		
+
+		
+
+		
+
+		// infinite line generator is \generated write your code inside this box
+
+		
+
+		
+
+		
+
+		/*
+
+		
+
+		Syntax for writting the code is use 
+
+		
+
+		else if (command == "<your command name>"){
+
+		
+
+			c++ code that should run	
+
+			
+
+	}
+
+	
+
+	
+
+	
+
+	for example :-
+
+	else if (command == "print:"){
+
+		
+
+		
+
+		char print[];
+
+		cin>>print;
+
+		cout<<print;
+
+		
+
+		
+
+	}
+
+	
+
+		
+
+		*/
+
+		
+
+		
+
+		if (command == "/developer_info"){
+
+			
+
+			cout<<endl<<"developer name : GHGLTGGAMER"<<endl;
+
+			cout<<"developer geder : male"<<endl<<endl;
+
+			
+
+		}
+
+		
+
+		
+
+		
+
+		else if (command == "/get_code_version"){
+
+			
+
+			cout<<endl<<endl<<"C++ 2023, day : 21, month 06"<<endl<<endl;
+
+			
+
+		} // read the syntax and write the code bellow this else if or this line
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+		
+
+	}
+
+}
